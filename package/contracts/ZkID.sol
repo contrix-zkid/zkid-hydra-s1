0ec354079a8a192f7e5d923583571eb24b6771c6
Mobius <chunloved@gmail.com>
2023-04-08 01:49:50 +0700

    feat: add zkid contract

---
+++ package/contracts/ZkID.sol
@@ -0,0 +1,73 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./IHydraS1Verifier.sol";
+
+contract ZkID is ERC721 {
+    constructor(address hydraS1Verifier_) ERC721("ZkID", "ZKID") {
+        hydraS1Verifier = IHydraS1Verifier(hydraS1Verifier_);
+    }
+
+    uint256 supply;
+    IHydraS1Verifier hydraS1Verifier;
+    // key: nullifier
+    mapping (uint256 => bool) isNullifierExpired;
+    mapping (uint256 => NFTMetadata) tokenInfo;
+
+    // Define the struct for your NFT's metadata
+    struct NFTMetadata {
+        uint256 tokenId;
+        address owner;
+        uint256[] cids; 
+    }
+    
+    // Create a new NFT
+    function createCredential(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[5] memory input
+    ) external {
+        // check proof
+        require(hydraS1Verifier.verifyProof(a, b, c, input), "Invalid Proof");
+        // mint new token if tokenId 
+        address mintTo = address(getMintTo(input[0]));
+        uint256 tokenId = getTokenId(input[0]);
+
+        if (tokenId == 2 ** 96 - 1) {
+            // uint256 tokenId = nfts.length;
+            _safeMint(mintTo, supply);
+            tokenId = supply;
+            tokenInfo[supply].tokenId = supply;
+            tokenInfo[supply].owner = mintTo;
+            tokenInfo[supply].cids.push(input[2]);
+
+            supply += 1; 
+        } else {
+            require(tokenId < supply, "Invalid TokenId");
+            tokenInfo[tokenId].cids.push(input[2]);
+        }
+    }
+
+    // Get the metadata of an NFT
+    function getNFTMetadata(uint256 _tokenId) external view returns (NFTMetadata memory) {
+        require(_exists(_tokenId), "Token ID does not exist");
+        return tokenInfo[_tokenId];
+    }
+
+    function getMintTo(uint256 number) public pure returns (bytes20) {
+        // Shift the uint256 value to the right by 96 bits to get the last 20 bytes
+        bytes20 converted = bytes20(uint160(number >> 96));
+        return converted;
+    }
+
+    function getTokenId(uint256 value) public pure returns (uint256) {
+        // Define a mask with the desired bits set to 1
+        uint256 mask = uint256(2 ** 96 - 1);
+
+        // Perform a bitwise AND with the mask to get the last 12 bytes
+        uint256 result = value & mask;
+
+        return result;
+    }
+}
